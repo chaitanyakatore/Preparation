6d9a4b2bf19cd8c2c7113a0c2f4dcce9c7550ce9
chaitanya <chaitanyakatore@gmail.com>
2026-02-14 20:43:57 +0530

    reverseArray using the recursion

--- DSA/reverseArrayRecursion.java
+++ DSA/reverseArrayRecursion.java
@@ -1,3 +1,23 @@
 public class reverseArrayRecursion {
     
+    public static void reverse(int l, int r, int[] arr){
+
+        if(l > r) return;
+
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        l++;
+        r--;
+        reverse(l, r, arr);
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        reverse(0, 4, arr);
+
+        for(int num: arr){
+            System.out.println(num);
+        }
+    }
+
 }
